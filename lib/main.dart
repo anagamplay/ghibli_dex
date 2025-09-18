1483199d0be8c89a368537400cfc7f18e3b1c785
anagamplay <anaclarasilvestre360@gmail.com>
2025-09-18 09:40:37 -0400

    implements clarity_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:clarity_flutter/clarity_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'app/modules/home/presentation/pages/home_page.dart';
 import 'app/themes/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await dotenv.load(fileName: ".env");
+  final projectId = dotenv.env['CLARITY_PROJECT_ID'] ?? 'CLARITY_PROJECT_ID';
+
+  final config = ClarityConfig(
+    projectId: projectId,
+    logLevel: LogLevel.Verbose,
+  );
+
+  runApp(ClarityWidget(
+    app: const MyApp(),
+    clarityConfig: config,
+  ));
 }
 
 class MyApp extends StatelessWidget {
